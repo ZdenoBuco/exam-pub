e3a20a2a54d6f746e4aa63aaa6860cb0fbded7c5
Zdenko Bučo <buco.zdeno@gmail.com>
2023-10-02 14:28:20 +0200

    add order model

--- src/main/java/com/example/exampub/models/Order.java
+++ src/main/java/com/example/exampub/models/Order.java
@@ -8,18 +8,15 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.Date;
-
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private Date lastActive;
-    private Date dateOfBirth;
-    private Double pocket;
+    private String productName;
+    private Integer Amount;
+    private double price;
 }
